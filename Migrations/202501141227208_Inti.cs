4db586971748aee6dc8918d2226884496f881dd3
Berat Öksüz <beratksz@icloud.com>
2025-01-14 15:28:19 +0300

    aa
    aa

---
+++ Migrations/202501141227208_Inti.cs
@@ -0,0 +1,47 @@
+﻿namespace MZDNETWORK.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Inti : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Tasks", "User_Id", "dbo.Users");
+            DropForeignKey("dbo.Tasks", "AssignedById", "dbo.Users");
+            DropForeignKey("dbo.Tasks", "AssignedToId", "dbo.Users");
+            DropIndex("dbo.Tasks", new[] { "AssignedById" });
+            DropIndex("dbo.Tasks", new[] { "AssignedToId" });
+            DropIndex("dbo.Tasks", new[] { "User_Id" });
+            DropTable("dbo.Tasks");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Tasks",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Description = c.String(),
+                        DueDate = c.DateTime(nullable: false),
+                        Priority = c.Int(nullable: false),
+                        Status = c.String(),
+                        CreatedDate = c.DateTime(nullable: false),
+                        UpdatedDate = c.DateTime(nullable: false),
+                        AssignedById = c.Int(nullable: false),
+                        AssignedToId = c.Int(nullable: false),
+                        User_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateIndex("dbo.Tasks", "User_Id");
+            CreateIndex("dbo.Tasks", "AssignedToId");
+            CreateIndex("dbo.Tasks", "AssignedById");
+            AddForeignKey("dbo.Tasks", "AssignedToId", "dbo.Users", "Id", cascadeDelete: true);
+            AddForeignKey("dbo.Tasks", "AssignedById", "dbo.Users", "Id", cascadeDelete: true);
+            AddForeignKey("dbo.Tasks", "User_Id", "dbo.Users", "Id");
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Net;
+using System.Threading.Tasks;
 using MZDNETWORK.Models;
 
 namespace MZDNETWORK.Models
@@ -22,7 +23,6 @@ public class User
     public string InternalEmail { get; set; }
     public string ExternalEmail { get; set; }
     public string Sicil { get; set; }
-
     public ICollection<UserInfo> UserInfo { get; set; }
     public bool IsPasswordChanged { get; set; }
 
